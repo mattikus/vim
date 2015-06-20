d8d447a546188da0968822206c1a9cc453237207
Matt Kemp <matt@mattikus.com>
2015-06-20 00:02:39 -0500

    Cleaned up bootstrapping process a bit.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-if [ ! -d autoload/plug.vim ]; then
-   curl -fLo autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+set -euo pipefail
+
+if which nvim &>/dev/null; then
+    vimprg=nvim
+    vimpath=$HOME/.nvim
+else
+    vimprg=vim
+    vimpath=$HOME/.vim
 fi
 
-if ! (vim --version | grep -q "2nd user vimrc file"); then
-    echo symlinking vimrc
-    [ -L $HOME/.vimrc ] || ln -sf $HOME/.vim/vimrc $HOME/.vimrc
+if [ ! -f ${vimpath}/autoload/plug.vim ]; then
+    curl -fLo ${vimpath}/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    if [[ "$vimprg" == "vim" ]] && \
+        ! ($vimprg --version | grep -q "2nd user vimrc file"); then
+        echo symlinking vimrc
+        [ -L $HOME/.vimrc ] || ln -sf $HOME/.vim/vimrc $HOME/.vimrc
+    fi
+
+    $vimprg +PlugInstall! +qall!
+else
+    $vimprg +PlugUpdate +qall!
 fi
-vim +colorscheme desert +PlugInstall! +qall!
